1a550355dae82d899ea96dc303edd45208203842
tioDaMariola <andarilhokenshin@gmail.com>
2017-08-26 02:47:27 -0300

    adding some functions

---
+++ index.js
@@ -0,0 +1,10 @@
+	function myPkmn(){
+		var userName = prompt("What's yout pokémon's name?","Enter it's name");
+		alert(userName+", I choose you!!!");
+		document.getElementById("message1").style.display = "block";
+		document.getElementById("magi").style.display = "block";
+	}
+	/*
+	function spash(){
+		document.getElementById("magi").style.margin-top="10px";
+	}  */
