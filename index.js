6b40fc5fd184d2873be7025b3274daebe0ab46a6
tioDaMariola <andarilhokenshin@gmail.com>
2017-08-26 04:17:38 -0300

    too tired. Almost no animation. Trying to understand timeouts and intervals

--- index.js
+++ index.js
@@ -3,8 +3,52 @@
 		alert(userName+", I choose you!!!");
 		document.getElementById("message1").style.display = "block";
 		document.getElementById("magi").style.display = "block";
+
+		
 	}
-	/*
-	function spash(){
-		document.getElementById("magi").style.margin-top="10px";
-	}  */
+	
+	function splash(){
+		document.getElementById("message1").style.display="none";
+/*
+		var x = 0;
+
+		time = setInterval(checkSplash,300);
+		
+		if(time == 300){
+			x++;
+		}
+
+		if(x==4){
+			clearInterval(time);
+		}
+*/
+splash1();
+
+			
+	}
+
+	function splash1(){
+		document.getElementById("message2").style.display="none";
+		document.getElementById("magi").style.marginTop = "10vh";
+		setTimeout(splash2,200);
+		setTimeout(showMessage,400);
+	}
+
+	function splash2(){
+		document.getElementById("magi").style.marginTop = "25vh";
+
+				
+	}
+
+	function showMessage(){
+		document.getElementById("message2").style.display="block";	
+	}
+
+	function checkSplash(){
+		if(document.getElementById("magi").style.marginTop == "25vh"){
+			document.getElementById("magi").style.marginTop = "10vh";
+		} else {
+			document.getElementById("magi").style.marginTop = "25vh";
+		}
+
+	}
